abfb50335918907d5115572b7a93511a83e51617
Nile Miller <nmiller@galileosuite.com>
2026-08-12 17:12:24 -0400

    add fonts and logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,41 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
- site: 'https://breezeway.nilemiller.dev/',
- trailingSlash: 'always'
+  site: "https://breezeway.nilemiller.dev/",
+  trailingSlash: "always",
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Equateur",
+      cssVariable: "--font-equateur",
+      fallbacks: ["Georgia", "serif"],
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/Equateur-Regular.woff2"],
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Manrope",
+      cssVariable: "--font-manrope",
+      fallbacks: ["system-ui", "sans-serif"]
+    },
+    {
+      provider: fontProviders.local(),
+      name: "JMH Typewriter",
+      cssVariable: "--font-jmh-typewriter",
+      fallbacks: ["monospace"],
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/JMH-Typewriter.regular.woff2"],
+          },
+        ],
+      },
+    },
+  ],
 });
